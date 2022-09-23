aba0f8562e2838d03092892631eb5c38a0dc6757
averywpx <averywpx@gmail.com>
2022-09-22 22:27:50 -0400

    change name to training_ht

--- gnomad_constraint/constraint/constraint_pipeline.py
+++ gnomad_constraint/constraint/constraint_pipeline.py
@@ -99,9 +99,9 @@ def main(args):
             ).write(training_ht_path.replace(".ht", "_y.ht"), overwrite=args.overwrite)
             logger.info("Done with creating training dataset.")
 
-        coverage_ht = hl.read_table(po_coverage_ht_path)
-        coverage_x_ht = hl.read_table(po_coverage_ht_path.replace(".ht", "_x.ht"))
-        coverage_y_ht = hl.read_table(po_coverage_ht_path.replace(".ht", "_y.ht"))
+        training_ht = hl.read_table(po_coverage_ht_path)
+        training_x_ht = hl.read_table(po_coverage_ht_path.replace(".ht", "_x.ht"))
+        training_y_ht = hl.read_table(po_coverage_ht_path.replace(".ht", "_y.ht"))
 
     finally:
         logger.info("Copying log to logging bucket...")
