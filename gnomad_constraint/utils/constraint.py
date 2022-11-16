443c540938ec7419b364d8a61c3382a7a706109d
averywpx <averywpx@gmail.com>
2022-11-16 13:56:19 -0500

    add draft

--- gnomad_constraint/utils/constraint.py
+++ gnomad_constraint/utils/constraint.py
@@ -1,6 +1,6 @@
 """Script containing utility functions used in the constraint pipeline."""
 
-from typing import Tuple
+from typing import Optional, Tuple
 
 import hail as hl
 from gnomad.utils.constraint import (
@@ -9,6 +9,12 @@ from gnomad.utils.constraint import (
     collapse_strand,
     count_variants_by_group,
     trimer_from_heptamer,
+)
+from gnomad.utils.filtering import (
+    filter_by_frequency,
+    filter_for_mu,
+    filter_to_autosomes,
+    remove_coverage_outliers,
 )
 from gnomad.utils.vep import (
     add_most_severe_csq_to_tc_within_vep_root,
@@ -204,3 +210,228 @@ def create_constraint_training_dataset(
         training_dataset_params=hl.struct(max_af=max_af, pops=pops)
     )
     return ht
+
+
+def calculate_mu_by_downsampling(
+    genome_ht: hl.Table,
+    raw_context_ht: hl.MatrixTable,
+    recalculate_all_possible_summary: bool = True,
+    recalculate_all_possible_summary_unfiltered: bool = False,
+    omit_methylation: bool = False,
+    count_singletons: bool = False,
+    remove_common_downsampled: bool = True,
+    remove_common_ordinary: bool = False,
+    grouping_variables: Optional[Tuple[str]] = (),
+    summary_file: str = None,
+) -> hl.Table:
+    """
+    _summary_.
+
+    :param genome_ht: _description_
+    :param raw_context_ht: _description_
+    :param recalculate_all_possible_summary: _description_, defaults to True
+    :param recalculate_all_possible_summary_unfiltered: _description_, defaults to False
+    :param omit_methylation: _description_, defaults to False
+    :param count_singletons: _description_, defaults to False
+    :param remove_common_downsampled: _description_, defaults to True
+    :param remove_common_ordinary: _description_, defaults to False
+    :param grouping_variables: _description_, defaults to ()
+    :param summary_file: _description_, defaults to None
+    :return: _description_
+    """
+    context_ht = filter_to_autosomes(remove_coverage_outliers(raw_context_ht))
+    genome_ht = filter_to_autosomes(remove_coverage_outliers(genome_ht))
+
+    if grouping_variables:
+        context_ht = context_ht.filter(
+            hl.all(
+                lambda x: x, [hl.is_defined(context_ht[x]) for x in grouping_variables]
+            )
+        )
+        genome_ht = genome_ht.filter(
+            hl.all(
+                lambda x: x, [hl.is_defined(genome_ht[x]) for x in grouping_variables]
+            )
+        )
+    else:
+        context_ht = filter_for_mu(context_ht)
+        genome_ht = filter_for_mu(genome_ht)
+
+    context_ht = context_ht.select(
+        "context", "ref", "alt", "methylation_level", *grouping_variables
+    )
+    genome_ht = genome_ht.select(
+        "context",
+        "ref",
+        "alt",
+        "methylation_level",
+        "freq",
+        "pass_filters",
+        *grouping_variables,
+    )
+
+    genome_join = genome_ht[context_ht.key]
+    if remove_common_downsampled:
+        ac_cutoff = 5
+        downsampling_level = 1000
+        freq_index = hl.eval(
+            hl.zip_with_index(genome_ht.freq_meta).find(
+                lambda f: (f[1].get("downsampling") == str(downsampling_level))
+                & (f[1].get("pop") == "global")
+                & (f[1].get("group") == "adj")
+                & (f[1].size() == 3)
+            )
+        )[0]
+        # In the denominator, only keep variants not in the genome dataset, or with AC <= ac_cutoff and passing filters
+        context_ht = context_ht.filter(
+            hl.is_missing(genome_join)
+            | (
+                (genome_join.freq[freq_index].AC <= ac_cutoff)
+                & genome_join.pass_filters
+            )
+        )
+        # Keep only AC <= ac_cutoff in numerator
+        genome_ht = genome_ht.filter(genome_ht.freq[freq_index].AC <= ac_cutoff)
+    elif remove_common_ordinary:
+        af_cutoff = 0.001
+        context_ht = context_ht.filter(
+            hl.is_missing(genome_join)
+            | ((genome_join.freq[0].AF <= af_cutoff) & genome_join.pass_filters)
+        )
+        # Keep only AF < af_cutoff in numerator
+        genome_ht = filter_by_frequency(
+            genome_ht, "below", frequency=af_cutoff, adj=True
+        )
+    else:
+        context_ht = context_ht.filter(
+            hl.is_missing(genome_join) | genome_join.pass_filters
+        )
+    genome_ht = genome_ht.filter(genome_ht.pass_filters)
+
+    if not summary_file:
+        summary_file = all_possible_summary_pickle
+    all_possible_dtype = count_variants_by_group(
+        context_ht,
+        omit_methylation=omit_methylation,
+        additional_grouping=grouping_variables,
+        return_type_only=True,
+    )
+    if recalculate_all_possible_summary:
+        all_possible = count_variants_by_group(
+            context_ht,
+            omit_methylation=omit_methylation,
+            additional_grouping=grouping_variables,
+        ).variant_count
+        with hl.hadoop_open(summary_file, "wb") as f:
+            pickle.dump(all_possible, f)
+    with hl.hadoop_open(summary_file, "rb") as f:
+        all_possible = pickle.load(f)
+
+    if recalculate_all_possible_summary_unfiltered:
+        all_possible_unfiltered = count_variants_by_group(
+            raw_context_ht.rows(), omit_methylation=omit_methylation
+        ).variant_count
+        all_possible_unfiltered = {
+            x: y
+            for x, y in list(all_possible_unfiltered.items())
+            if x.context is not None
+        }
+        with hl.hadoop_open(all_possible_summary_unfiltered_pickle, "wb") as f:
+            pickle.dump(all_possible_unfiltered, f)
+    # Uncomment this line and next few commented lines to back-calculate total_mu from the old mutation rate dataset
+    # all_possible_unfiltered = load_all_possible_summary(filtered=False)
+
+    genome_ht = count_variants_by_group(
+        genome_ht,
+        count_downsamplings=["global", "nfe", "afr"],
+        count_singletons=count_singletons,
+        additional_grouping=grouping_variables,
+        omit_methylation=omit_methylation,
+    )
+
+    old_mu_data = get_old_mu_data()
+    ht = genome_ht.annotate(
+        possible_variants=hl.literal(all_possible, dtype=all_possible_dtype)[
+            genome_ht.key
+        ],
+        # possible_variants_unfiltered=hl.literal(all_possible_unfiltered, dtype=all_possible_dtype)[genome_ht.key],
+        old_mu_snp=old_mu_data[
+            hl.struct(context=genome_ht.context, ref=genome_ht.ref, alt=genome_ht.alt)
+        ].mu_snp,
+    )
+    ht = ht.persist()
+    total_bases = ht.aggregate(hl.agg.sum(ht.possible_variants)) // 3
+    # total_bases_unfiltered = ht.aggregate(hl.agg.sum(ht.possible_variants_unfiltered)) // 3
+    # total_mu = ht.aggregate(hl.agg.sum(ht.old_mu_snp * ht.possible_variants_unfiltered) / total_bases_unfiltered)
+    total_mu = 1.2e-08
+
+    correction_factors = ht.aggregate(
+        total_mu / (hl.agg.array_sum(ht.downsampling_counts_global) / total_bases)
+    )
+    correction_factors_nfe = ht.aggregate(
+        total_mu / (hl.agg.array_sum(ht.downsampling_counts_nfe) / total_bases)
+    )
+    correction_factors_afr = ht.aggregate(
+        total_mu / (hl.agg.array_sum(ht.downsampling_counts_afr) / total_bases)
+    )
+
+    ht = annotate_mutation_type(
+        ht.annotate(
+            downsamplings_frac_observed=ht.downsampling_counts_global
+            / ht.possible_variants,
+            downsamplings_mu_snp=hl.literal(correction_factors)
+            * ht.downsampling_counts_global
+            / ht.possible_variants,
+            downsamplings_mu_nfe=hl.literal(correction_factors_nfe)
+            * ht.downsampling_counts_nfe
+            / ht.possible_variants,
+            downsamplings_mu_afr=hl.literal(correction_factors_afr)
+            * ht.downsampling_counts_afr
+            / ht.possible_variants,
+        )
+    )
+    downsamplings = list(map(lambda x: x[1], get_downsamplings(ht)))
+    index_1kg = downsamplings.index(1000)
+    return ht.annotate(
+        proportion_observed_1kg=ht.downsampling_counts_global[index_1kg]
+        / ht.possible_variants,
+        proportion_observed=ht.variant_count / ht.possible_variants,
+        mu_snp=ht.downsamplings_mu_snp[index_1kg],
+        mu_snp_nfe=ht.downsamplings_mu_nfe[index_1kg],
+        mu_snp_afr=ht.downsamplings_mu_afr[index_1kg],
+    )
+
+
+# Data loads
+def get_old_mu_data() -> hl.Table:
+    """
+    _summary_.
+
+    :return: _description_
+    """
+    old_mu_data = hl.import_table(
+        f"{root}/old_exac_data/fordist_1KG_mutation_rate_table.txt",
+        delimiter=" ",
+        impute=True,
+    )
+    return old_mu_data.transmute(
+        context=old_mu_data["from"], ref=old_mu_data["from"][1], alt=old_mu_data.to[1]
+    ).key_by("context", "ref", "alt")
+
+
+def get_downsamplings(ht):
+    """
+    _summary_.
+
+    :param ht: _description_
+    :return: _description_
+    """
+    freq_meta = ht.freq_meta.collect()[0]
+    downsamplings = [
+        (i, int(x.get("downsampling")))
+        for i, x in enumerate(freq_meta)
+        if x.get("group") == "adj"
+        and x.get("pop") == "global"
+        and x.get("downsampling") is not None
+    ]
+    return downsamplings
