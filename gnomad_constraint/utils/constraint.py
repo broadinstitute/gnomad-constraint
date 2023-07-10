aaab67541af410754fcaf6fea07255399d6545f0
klaricch <kristen@broadinstitute.org>
2023-07-10 14:30:20 -0400

    black

--- gnomad_constraint/utils/constraint.py
+++ gnomad_constraint/utils/constraint.py
@@ -908,6 +908,6 @@ def annotate_context_ht(
         gerp=gerp_ht[ht.locus].S,
     )
 
-    ht  = ht.annotate(gerp = hl.if_else(hl.is_missing(ht.gerp), 0, ht.gerp))
-    
+    ht = ht.annotate(gerp=hl.if_else(hl.is_missing(ht.gerp), 0, ht.gerp))
+
     return ht
