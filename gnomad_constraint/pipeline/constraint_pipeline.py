68e1dd346d1736cb7d7950996ad0e4ad070d66b2
klaricch <kristen@broadinstitute.org>
2023-12-13 10:16:56 -0500

    Update gnomad_constraint/pipeline/constraint_pipeline.py
    Co-authored-by: Katherine Chao <kchao@broadinstitute.org>

--- gnomad_constraint/pipeline/constraint_pipeline.py
+++ gnomad_constraint/pipeline/constraint_pipeline.py
@@ -940,7 +940,7 @@ if __name__ == "__main__":
         help=(
             "Lower value at which the raw z-score is considered an outlier for"
             " synonymous variants. Values below this threshold will be considered"
-            " outliers. Defualt is -8.0."
+            " outliers. Default is -8.0."
         ),
         type=float,
         default=-8.0,
