c7a7cef337caa8c5c574b30dda7ce1c4c87346a9
klaricch <kristen@broadinstitute.org>
2024-03-01 14:04:11 -0500

    change paths

--- gnomad_constraint/pipeline/constraint_pipeline.py
+++ gnomad_constraint/pipeline/constraint_pipeline.py
@@ -514,11 +514,8 @@ def main(args):
 
             # Combine Tables of expected variant counts at autosomes/pseudoautosomal
             # regions, chromosome X, and chromosome Y sites.
-            # hts = [getattr(res, f"apply_{r}_ht").ht() for r in regions]
-            # union_ht = hts[0].union(*hts[1:])
-            union_ht = hl.read_table(
-                "gs://gnomad-kristen/constraint/apply_with_rank.ht"
-            )
+            hts = [getattr(res, f"apply_{r}_ht").ht() for r in regions]
+            union_ht = hts[0].union(*hts[1:])
             union_ht = union_ht.repartition(args.compute_constraint_metrics_partitions)
             union_ht = union_ht.checkpoint(
                 new_temp_file(prefix="constraint_apply_union", extension="ht")
@@ -551,8 +548,7 @@ def main(args):
                 include_os=not version_4_and_above,
                 use_mane_select_over_canonical=version_4_and_above,
             ).select_globals("version", "apply_model_params", "sd_raw_z").write(
-                "gs://gnomad-kristen/constraint/extra_annotations.ht"
-                # res.constraint_metrics_ht.path, overwrite=overwrite
+                res.constraint_metrics_ht.path, overwrite=overwrite
             )
             logger.info("Done with computing constraint metrics.")
 
@@ -561,11 +557,9 @@ def main(args):
             res.check_resource_existence()
             logger.info("Exporting constraint tsv...")
 
-            # ht = res.constraint_metrics_ht.ht()
-            ht = hl.read_table("gs://gnomad-kristen/constraint/extra_annotations.ht")
+            ht = res.constraint_metrics_ht.ht()
             ht = ht.flatten()
-            # ht.export(res.constraint_metrics_tsv)
-            ht.export("gs://gnomad-kristen/constraint/extra_annotations.tsv")
+            ht.export(res.constraint_metrics_tsv)
 
     finally:
         logger.info("Copying log to logging bucket...")
