217f29e6598951e335185fdac931834ff36d1909
klaricch <kristen@broadinstitute.org>
2024-04-09 11:08:44 -0400

    add high_cov_defintion to globals

--- gnomad_constraint/utils/constraint.py
+++ gnomad_constraint/utils/constraint.py
@@ -556,6 +556,7 @@ def apply_models(
             pops=pops,
             plateau_models=plateau_models,
             coverage_model=coverage_model_global,
+            high_cov_definition=high_cov_definition,
             downsamplings=downsamplings if downsamplings else "None",
         )
     )
