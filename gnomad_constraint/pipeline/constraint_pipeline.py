b7d34be110ad0b4ec7c5186e371d780789725985
klaricch <kristen@broadinstitute.org>
2024-05-10 15:38:03 -0400

    paths

--- gnomad_constraint/pipeline/constraint_pipeline.py
+++ gnomad_constraint/pipeline/constraint_pipeline.py
@@ -568,7 +568,8 @@ def main(args):
             res.check_resource_existence()
             logger.info("Exporting constraint tsv...")
 
-            ht = res.constraint_metrics_ht.ht()
+            #ht = res.constraint_metrics_ht.ht()
+            ht = hl.read_table("gs://gnomad-kristen/constraint/metrics_ds4.ht")
             # If downsamplings per genetic ancestry group are present, export
             # downsamplings to a separate tsv and drop from the main metrics tsv.
             if pops:
@@ -587,7 +588,8 @@ def main(args):
                 # Export separate downsampling Table.
                 downsampling_ht.export(res.downsampling_constraint_metrics_tsv)
             ht = ht.flatten()
-            ht.export(res.constraint_metrics_tsv)
+            #ht.export(res.constraint_metrics_tsv)
+            ht.export("gs://gnomad-kristen/constraint/metrics_ds6.tsv")
 
     finally:
         logger.info("Copying log to logging bucket...")
