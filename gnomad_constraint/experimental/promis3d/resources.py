278cb86f319e9076daf9d036403e88e3c5e7156b
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2025-05-28 09:53:49 -0600

    Testing adding annotations

--- gnomad_constraint/experimental/promis3d/resources.py
+++ gnomad_constraint/experimental/promis3d/resources.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 VERSIONS = ["2.1.1", "4.1"]
 """Possible gnomAD versions for the promis3d pipeline."""
 
-CURRENT_VERSION = "2.1.1"
+CURRENT_VERSION = "4.1"
 """Current gnomAD version for the promis3d pipeline."""
 
 GENCODE_VERSION_MAP = {
@@ -109,7 +109,7 @@ def get_af2_ht(
     :param test: Whether to use a tmp path for testing. Default is False.
     :return: GENCODE sequences Hail Table resource.
     """
-    return TableResource(f"{get_promis3d_root(version, test)}/preprocessed_data/af2.ht")
+    return TableResource(f"{get_promis3d_root('2.1.1', test)}/preprocessed_data/af2.ht")
 
 
 def get_af2_dist_ht(
@@ -124,7 +124,7 @@ def get_af2_dist_ht(
     :return: AlphaFold2 distance matrix Hail Table resource.
     """
     return TableResource(
-        f"{get_promis3d_root(version, test)}/preprocessed_data/af2_dist.ht"
+        f"{get_promis3d_root('2.1.1', test)}/preprocessed_data/af2_dist.ht"
     )
 
 
@@ -140,7 +140,7 @@ def get_af2_plddt_ht(
     :return: AlphaFold2 pLDDT Hail Table resource.
     """
     return TableResource(
-        f"{get_promis3d_root(version, test)}/preprocessed_data/af2_plddt.ht"
+        f"{get_promis3d_root('2.1.1', test)}/preprocessed_data/af2_plddt.ht"
     )
 
 
@@ -156,7 +156,7 @@ def get_af2_pae_ht(
     :return: AlphaFold2 pAE Hail Table resource.
     """
     return TableResource(
-        f"{get_promis3d_root(version, test)}/preprocessed_data/af2_pae.ht"
+        f"{get_promis3d_root('2.1.1', test)}/preprocessed_data/af2_pae.ht"
     )
 
 
@@ -217,12 +217,17 @@ def get_obs_exp_ht(version: str = CURRENT_VERSION) -> TableResource:
     :param version: Version of gnomAD to use. Default is `CURRENT_VERSION`.
     :return: Observed/expected Hail Table resource.
     """
-    # TODO: Change this path when moved to a more permanent location, and add support
-    #  for v4.
-    return TableResource(
-        # "gs://regional_missense_constraint/model/2.1.1/7/context_coding_snps_annot.ht"
-        "gs://gnomad/v2.1.1/constraint/temp/gnomad.v2.1.1.per_base_expected.ht"
-    )
+    # TODO: Change this path when moved to a more permanent location.
+    if version == "2.1.1":
+        return TableResource(
+            "gs://gnomad/v2.1.1/constraint/temp/gnomad.v2.1.1.per_base_expected.ht"
+        )
+    elif version == "4.1":
+        return TableResource(
+            "gs://gnomad/v4.1/constraint_coverage_corrected/apply_models/transcript_consequences/gnomad.v4.1.per_variant_expected.coverage_corrected.ht"
+        )
+    else:
+        raise ValueError(f"Invalid version: {version}")
 
 
 def get_greedy_ht(
@@ -251,3 +256,123 @@ def get_forward_ht(
     :return: Promis3D forward algorithm Hail Table resource.
     """
     return TableResource(f"{get_promis3d_root(version, test)}/promis3D_forward.ht")
+
+
+def get_forward_annotation_ht(
+    name: str,
+    version: str = CURRENT_VERSION,
+    test: bool = False,
+) -> TableResource:
+    """
+    Get PROMIS3D forward algorithm annotation Hail Table resource.
+
+    :param name: Annotation type ('per_variant', 'per_missense_variant', 'per_residue',
+        or 'per_promis3d_region').
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :param test: Whether to use a tmp path for testing. Default is False.
+    :return: PROMIS3D annotated forward algorithm Hail Table resource.
+    """
+    return TableResource(
+        f"{get_promis3d_root(version, test)}/promis3D_forward.{name}.annotated.5_28_25.ht"
+    )
+
+
+########################################################################################
+# The following functions are for external resources.
+########################################################################################
+def get_kaplanis_variants() -> str:
+    """
+    Get Kaplanis annotated variants file path.
+
+    :return: File path to Kaplanis annotated variants.
+    """
+    return "gs://gnomad-julia/kaplanis_variants_annotated_2024-05-15.txt"
+
+
+def get_kaplanis_sig_variants() -> str:
+    """
+    Get Kaplanis significant variants file path.
+
+    :return: File path to Kaplanis significant variants.
+    """
+    return "gs://gnomad-julia/promis3d/kaplanis_variants_sig.txt"
+
+
+def get_kaplanis_variants_annotated_ht() -> TableResource:
+    """
+    Get processed Hail Table path for Kaplanis annotated de novo missense variants.
+
+    This is the output of the `process_kaplanis_variants_ht` function, containing
+    lifted GRCh38 loci and relevant annotations.
+
+    :return: Hail Table resource path for processed Kaplanis missense variants.
+    """
+    return TableResource("gs://gnomad-julia/promis3d/kaplanis_variants_annotated.ht")
+
+
+def get_interpro_annotations(version: str = CURRENT_VERSION) -> str:
+    """
+    Get Ensembl BioMart export InterPro annotations file path.
+
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: File path to InterPro annotations.
+    """
+    return f"gs://gnomad/v{version}/constraint/resources/ensembl_biomart_export_interpro.txt"
+
+
+def get_cosmis_score_tsv(model: str, version: str = CURRENT_VERSION) -> str:
+    """
+    Get COSMIS scores TSV path for a specified structure model.
+
+    :param model: Structure model source ('alphafold', 'swiss_model', or 'pdb').
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: COSMIS scores TSV file path.
+    """
+    return f"gs://gnomad/v{version}/constraint/resources/cosmis_scores_{model}.tsv.gz"
+
+
+def get_cosmis_score_ht(model: str, version: str = CURRENT_VERSION) -> TableResource:
+    """
+    Get COSMIS Hail Table resource for a specified structure model.
+
+    :param model: Structure model source ('alphafold', 'swiss_model', or 'pdb').
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: COSMIS scores Hail Table resource.
+    """
+    return TableResource(
+        f"gs://gnomad/v{version}/constraint/resources/cosmis_scores_{model}.ht"
+    )
+
+
+def get_all_rmc_ht(version: str = CURRENT_VERSION) -> TableResource:
+    """
+    Get Hail Table resource with all regional missense constraint (RMC) scores.
+
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: RMC Hail Table resource.
+    """
+    return TableResource(f"gs://gnomad/v{version}/constraint/resources/all_rmc.ht")
+
+
+def get_context_preprocessed_ht(version: str = CURRENT_VERSION) -> TableResource:
+    """
+    Get preprocessed context Hail Table resource.
+
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: Context preprocessed Hail Table resource.
+    """
+    return TableResource(
+        f"gs://gnomad/v{version}/constraint_coverage_corrected/preprocessed_data/gnomad.v{version}.context.preprocessed.ht"
+    )
+
+
+def get_constraint_metrics_ht(version: str = CURRENT_VERSION) -> TableResource:
+    """
+    Get coverage-corrected constraint metrics Hail Table resource.
+
+    :param version: gnomAD version to use. Default is `CURRENT_VERSION`.
+    :return: Constraint metrics Hail Table resource.
+    """
+    return TableResource(
+        f"gs://gnomad/v{version}/constraint_coverage_corrected/metrics/transcript_consequences/gnomad.v{version}.constraint_metrics.coverage_corrected.ht"
+    )
